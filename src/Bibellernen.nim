04195faecdc07a54c827199c6bb30bcc1d7ce393
ROKAS GARMASUKIS <ROKAS.GARMASUKIS@GMAIL.COM>
2022-11-12 12:22:50 +0100

    Implement server

--- src/Bibellernen.nim
+++ src/Bibellernen.nim
@@ -1,12 +1,29 @@
+import jester, asyncdispatch
+
 import models/userModel
 import ./database/database
-import os, bcrypt
+import bcrypt
 
-echo("Hello, Rokas!")
 
-var user: User
-user = User(username: "Rokas", password: "secret", salt: "salt")
+
+# var salt = genSalt(10)
+# var password = "secret"
+
+# var hashedPassword = hash(password, salt)
+
+# var user: User
+# user = User(username: "Rokas", password: hashedPassword, salt: salt)
 
 var db = newDatabase()
 
-createUser(db, user)
+# createUser(db, user)
+# loginUser(db, "Rokas", "secret")
+
+routes:
+  get "/@name":
+    resp db.findUserId(@"name")
+
+  get "/":
+    resp "index"
+
+runForever()
--- src/models/userModel.nim
+++ src/models/userModel.nim
@@ -1,5 +1,6 @@
 import db_sqlite
 import ../database/database
+import bcrypt
 
 type
   User* = object
@@ -20,5 +21,24 @@ proc setupUser*(database: Database) =
   """)
 
 proc createUser*(database: Database, user: User) =
+  # check for uniqueness
   database.db.exec(sql"INSERT INTO User (username, password, salt) VALUES (?, ?, ?);", user.username, user.password, user.salt)
 
+
+proc loginUser*(database: Database, username: string, password: string) =
+  let userInfo = database.db.getRow(sql"SELECT * FROM User WHERE username = ?;", username)
+  echo(userInfo.repr)
+  if userInfo[1].len == 0:
+    echo "no user found"
+  elif userInfo[2] == hash(password, userInfo[3]):
+    echo("User found")
+  else:
+    echo("Wrong password")
+
+proc findUserId*(database: Database, username: string): string =
+  let userId = database.db.getValue(sql"SELECT id FROM User WHERE username = ?", username)
+  result = ""
+  if userId.len == 0:
+    result.add("not found")
+  else:
+    result.add("User ID: " & userId)
